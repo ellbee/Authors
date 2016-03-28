08823a6cd879707acd547302fa3c5e009df4ce7c
Lee Bannard <l_bannard@yahoo.co.uk>
2016-03-28 16:31:00 +0100

    Remove some functions and variables

--- lib/authors.ex
+++ lib/authors.ex
@@ -1,6 +1,6 @@
 defmodule Authors do
   use HTTPoison.Base
-  
+
   def process_response_body(body) do
     body
     |> Poison.decode!
@@ -11,44 +11,40 @@ defmodule Authors do
     |> Map.get(:body)
     |> Enum.map(&Map.get(&1, "url"))
   end
-  
-  def get_name_and_email(contributor_url, user, password) do
+
+  def request_contributor(contributor_url, user, password) do
     String.replace(contributor_url, ~r(https://), "https://" <> user <> ":" <> password <> "@")
     |> Authors.get!
     |> Map.get(:body)
   end
-  
-  def request_contributor_details(contributor_urls, user, password) do
+
+  def request_contributors(contributor_urls, user, password) do
     contributor_urls
-    |> Enum.map(&Task.async(fn -> get_name_and_email(&1, user, password) end))
+    |> Enum.map(&Task.async(fn -> request_contributor(&1, user, password) end))
     |> Enum.map(&Task.await(&1, 10000))
   end
 
-  def contributor_to_string(contributor) do
-    %{"name" => name, "email" => email, "url" => url, "login" => login} = contributor
+  def contributor_to_string(%{"name" => name, "email" => email, "url" => url, "login" => login}) do
     email = if email, do: " <" <> email <> ">", else: ""
     (name || login) <> email <> " (" <> url <> ")"
   end
-  
+
   def write_to_authors_file(contributors_list) do
     contributor_list = contributors_list
     |> Enum.map(&contributor_to_string(&1))
     |> Enum.join("\n")
-    
+
     File.mkdir_p!("out")
     File.cd!("out", fn ->
       File.write!("AUTHORS", contributor_list)
     end)
   end
 
-  def make_authors_file(repo, user, password) do
-    request_contributor_urls(repo, user, password)
-    |> request_contributor_details(user, password)
-    |> write_to_authors_file
-  end
-
   def main(args) do
     {[user: user, password: password], [repo], []} = OptionParser.parse(args)
-    make_authors_file(repo, user, password)
+
+    request_contributor_urls(repo, user, password)
+    |> request_contributors(user, password)
+    |> write_to_authors_file
   end
 end
